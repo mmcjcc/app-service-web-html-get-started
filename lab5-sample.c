4a228ab98cb63496bbe718c94d6660eed586958f
Jason Cohen <jcohen@mmcjcc.com>
2026-09-20 21:47:06 -0400

    Reset demo fork to a clean starting state

--- lab5-sample.c
+++
@@ -1,8 +0,0 @@
-/* CYSC-620 Lab 5 sample - FIXED: gets() replaced with a bounded read. */
-#include <stdio.h>
-
-int main() {
-    char src[40];
-    fgets(src, sizeof(src), stdin);
-    return 0;
-}
